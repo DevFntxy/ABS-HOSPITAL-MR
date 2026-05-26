260d0e188c0c07e479683763ec3a9de739c4101a
Erick Matias Granillo Mejia <230045@utxicotepec.edu.mx>
2026-05-26 13:15:24 -0600

    Fixes
    Api terminada Semi

--- api/api-hibrida/core/config.py
+++ api/api-hibrida/core/config.py
@@ -1,11 +1,11 @@
 MYSQL_CONFIG: dict = {
     "host":     "localhost",
     "user":     "root",
-    "password": "1234",
-    "database": "hospital_230260",
-    "port":     3306,
+    "password": "12345678",
+    "database": "hospital_matricula",
+    "port":     3307,
 }
 
-MONGO_URI  = "mongodb://localhost:27017"
-MONGO_DB   = "hospital_230260_nosql"
+MONGO_URI  = "mongodb://localhost:27017/"
+MONGO_DB   = "hospital_matricula"
 MONGO_COLL = "equipamientos_especificaciones"
